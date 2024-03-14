08216948d3a1ee672c4c2009cf1e83cf0813bc85
vijaylaxmi hanamshetty <vijaylaxmihanamshetty38@.in>
2024-03-14 12:42:04 +0530

    first commit

---
+++ src/pages/Pagenotfound.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../Components/Layout/Layout";
+
+
+const Pagenotfound = () => {
+  return (
+   <Layout>
+    <h1>Page is not found</h1>
+   </Layout>
+  );
+};
+
+export default Pagenotfound;
